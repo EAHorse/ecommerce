e03e6e73b3d3e850e856e4a669d73b5ab194d61c
Alexis Caballero <out_ecaballero@findep.com.mx>
2024-07-02 01:26:19 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Titulo from './components/Titulo'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <Titulo></Titulo>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
